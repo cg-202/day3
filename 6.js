0831fd9aa5f72f8eaf198c871f0adc5fd12ab6e1
santosh <santosh.ece06@gmail.com>
2021-06-25 12:36:51 +0530

    6

---
+++ 6.js
@@ -0,0 +1,12 @@
+function addNewBlock() {
+  let inputRef = document.querySelector("#inputBox");
+  let value = inputRef.value;
+
+  // ADDING NEW BLOCKS
+  let parentRef = document.querySelector("#parent");
+  let newblock = `<div>${value}</div>`;
+  parentRef.innerHTML = parentRef.innerHTML + newblock;
+
+  // cleaar the box
+  inputRef.value = "";
+}
