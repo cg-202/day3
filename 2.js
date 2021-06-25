81242f8bf7c8270de0cb278a60e003cdb068b81d
santosh <santosh.ece06@gmail.com>
2021-06-25 12:02:26 +0530

    html, css, js

--- 2.js
+++ 2.js
@@ -3,3 +3,26 @@ console.log("Second Program!!");
 function helloWorld() {
   console.log("Helloo Worlddd");
 }
+
+function helloDom() {
+  // console.log(document);
+  let elementRef = document.querySelector("#id1");
+
+  // Access the style and Upadte the color
+  elementRef.style.color = "red";
+}
+
+function red() {
+  let elementRef = document.querySelector("#id1");
+  elementRef.style.color = "red";
+}
+
+function green() {
+  let elementRef = document.querySelector("#id1");
+  elementRef.style.color = "green";
+}
+
+function blue() {
+  let elementRef = document.querySelector("#id1");
+  elementRef.style.color = "blue";
+}
